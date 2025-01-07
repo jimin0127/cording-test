0c3ec14732564fae149e0f3a6b5acff7afcee3d1
jimin0127 <ckdgh4662@naver.com>
2025-01-07 19:51:32 +0900

    valid palindrome

---
+++ javaArgorithmInterview/validPalindrome.kt
@@ -0,0 +1,30 @@
+package javaArgorithmInterview
+
+fun isPalindrome(s: String): Boolean {
+    var start = 0
+    var end = s.length - 1
+
+    while (start < end) {
+        when {
+            !Character.isLetterOrDigit(s[start]) -> start++
+            !Character.isLetterOrDigit(s[end]) -> end--
+            else -> {
+                if (Character.toLowerCase(s[start]) != Character.toLowerCase(s[end])) {
+                    return false
+                }
+
+                start++
+                end--
+            }
+        }
+    }
+
+    return true
+}
+
+fun main() {
+    println(isPalindrome("Do geese see God?"))
+    println(isPalindrome("소주 만병만 주소"))
+    println(isPalindrome("no palindrome"))
+}
+
