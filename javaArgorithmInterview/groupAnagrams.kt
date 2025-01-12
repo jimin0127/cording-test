69f6fcab11ba4e316081506f57b59a8836d02094
jimin0127 <ckdgh4662@naver.com>
2025-01-12 23:27:12 +0900

    group anagrams

---
+++ javaArgorithmInterview/groupAnagrams.kt
@@ -0,0 +1,21 @@
+package javaArgorithmInterview
+
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    val results = mutableMapOf<String, MutableList<String>>()
+
+    for (s in strs) {
+        val sortedStr = s.toCharArray().sorted().joinToString("")
+        results.getOrPut(sortedStr) { mutableListOf() }
+        results[sortedStr]!!.add(s)
+    }
+
+//    return listOf(results.keys.toList())
+
+    return ArrayList<List<String>>(results.values)
+}
+
+fun main() {
+    val input = arrayOf("eat", "tea", "tan", "ate", "ant", "cat")
+
+    println(groupAnagrams(input)) // [[eat, tea, ate], [tan, ant], [cat]]
+}
