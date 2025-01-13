d8b1e5f16db67a5d61df86e3923c943a3b17c699
jimin0127 <ckdgh4662@naver.com>
2025-01-13 20:38:56 +0900

    longest palindrome substring

---
+++ javaArgorithmInterview/longestPalindromeSubstring.kt
@@ -0,0 +1,39 @@
+package javaArgorithmInterview
+
+var left = 0
+var maxLen = 0
+
+fun extendPalindrome(s: String, j: Int, k: Int) {
+    var l = j
+    var r = k
+
+    while (l >= 0 && r < s.length && s[l] == s[r]) {
+        l--
+        r++
+    }
+
+    if (maxLen < r - l - 1) {
+        left = l + 1
+        maxLen = r - l - 1
+    }
+}
+
+fun longestPalindrome(s: String): String {
+    val len = s.length
+
+    if (len < 2) return s
+
+    for (i in 0 until len - 1) {
+        // 2칸 two pointer
+        extendPalindrome(s, i, i + 1)
+        // 3칸 two pointer
+        extendPalindrome(s, i, i + 2)
+    }
+
+    return s.substring(left, left + maxLen)
+}
+
+fun main() {
+    val input = "dcbabcdd"
+    println(longestPalindrome(input)) // dcbabcd
+}
