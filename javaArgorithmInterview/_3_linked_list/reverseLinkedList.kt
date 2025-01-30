f571e3d37d1e8a22378dd447b8eb5d432806962c
jimin0127 <ckdgh4662@naver.com>
2025-01-30 22:49:50 +0900

    reverse linked list

---
+++ javaArgorithmInterview/_3_linked_list/reverseLinkedList.kt
@@ -0,0 +1,37 @@
+package javaArgorithmInterview._3_linked_list
+
+fun reverseLinkedList(head: ListNode?): ListNode? {
+    var prev: ListNode? = null
+    var node = head
+
+    while (node != null) {
+        val next = node.next
+        node.next = prev
+        prev = node
+        node = next
+    }
+
+    return prev
+}
+
+
+fun main() {
+    val input = ListNode(value = 1)
+        .apply { next = ListNode(value = 2)
+            .apply { next = ListNode(value = 3)
+                .apply { next = ListNode(value = 4)
+                    .apply { next = ListNode(value = 5)
+                        .apply { next = ListNode(value = 6) }
+                    }
+                }
+            }
+        }
+
+    var result = reverseLinkedList(input)
+    while (result != null) {
+        println(result.value)
+
+        result = result.next
+    }
+    // 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> null
+}
