398fae8f1ce5f944439015317aeabe33e2dd2aff
jimin0127 <ckdgh4662@naver.com>
2025-01-31 20:39:19 +0900

    swap nodes in pairs

---
+++ javaArgorithmInterview/_3_linked_list/swapNodesInPairs.kt
@@ -0,0 +1,33 @@
+package javaArgorithmInterview._3_linked_list
+
+fun swapNodesInPairs(head: ListNode?): ListNode? {
+    if (head?.next != null) {
+        val p = head.next!!
+        head.next = swapNodesInPairs(head.next!!.next)
+        p.next = head
+        return p
+    }
+
+    return head
+}
+
+fun main() {
+    val input = ListNode(value = 1)
+        .apply { next = ListNode(value = 2)
+            .apply { next = ListNode(value = 3)
+                .apply { next = ListNode(value = 4)
+                    .apply { next = ListNode(value = 5)
+                        .apply { next = ListNode(value = 6) }
+                    }
+                }
+            }
+        }
+
+    var result = swapNodesInPairs(input)
+    while (result != null) {
+        println(result.value)
+
+        result = result.next
+    }
+    // 2 -> 1 -> 4 -> 3 -> 6 -> 5
+}
